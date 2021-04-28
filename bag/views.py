15ab5085b2b53dc2b2560fb7e3d6f43804a6a2bb
scottsimpson91 <scottsimpson7@googlemail.com>
2021-04-28 14:13:48 +0000

    FEAT: Simplify order form, create add to bag views and urls

--- bag/views.py
+++ bag/views.py
@@ -16,9 +16,9 @@ def add_to_bag(request, item_id):
 
     if item_id in list(bag.keys()):
         bag[item_id] += quantity
+
     else:
         bag[item_id] = quantity
 
     request.session['bag'] = bag
-    print(request.session['bag'])
     return redirect(redirect_url)
