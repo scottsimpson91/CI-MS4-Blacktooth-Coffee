3b37d5a98cd44031252f24169e40c091890e0afa
scottsimpson91 <scottsimpson7@googlemail.com>
2021-07-15 10:25:21 +0000

    MAINT: Refactor/neaten up code and change Twilio API Key

--- products/models.py
+++ products/models.py
@@ -18,10 +18,12 @@ class Category(models.Model):
 
 
 class Product(models.Model):
-    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
+    category = models.ForeignKey('Category', null=True,
+                                 blank=True, on_delete=models.SET_NULL)
     sku = models.CharField(max_length=254, null=True, blank=True)
     name = models.CharField(max_length=254)
-    rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
+    rating = models.DecimalField(max_digits=6, decimal_places=2,
+                                 null=True, blank=True)
     origin = CountryField(blank_label='Country *', null=False, blank=False)
     region = models.CharField(max_length=254, null=True, blank=True)
     variety = models.CharField(max_length=254, null=True, blank=True)
