240ccf945f8860a02008385136cb684bc7742c95
scottsimpson91 <scottsimpson7@googlemail.com>
2021-08-27 09:49:15 +0000

    FEAT: Add regex to comment model

--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.validators import RegexValidator
 
 
 class Post(models.Model):
@@ -17,9 +18,9 @@ class Post(models.Model):
 
 class Comment(models.Model):
     post = models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE)
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255, validators=[RegexValidator(regex='^.{3}$', message='Minimum length of 3 characters', code='nomatch')])
     email = models.EmailField()
-    body = models.TextField()
+    body = models.TextField(validators=[RegexValidator(regex='^.{5}$', message='Minimum length of 5 characters', code='nomatch')])
     date_added = models.DateTimeField(auto_now_add=True)
 
     class Meta:
