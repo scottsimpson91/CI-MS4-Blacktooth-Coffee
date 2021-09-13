1adbb954b8d686a0f0f76098c54078a24af5cc99
scottsimpson91 <scottsimpson7@googlemail.com>
2021-09-13 10:36:14 +0000

    FEAT: Change edit post functionality to make it easier to use

--- blog/forms.py
+++ blog/forms.py
@@ -8,42 +8,6 @@ class BlogForm(forms.ModelForm):
         model = Post
         fields = ['title', 'slug', 'intro', 'body']
 
-    def clean_title(self):
-        title = self.cleaned_data.get('title')
-
-        for instance in Post.objects.all():
-            if instance.title == title:
-                raise forms.ValidationError(title + ' already exists.\
-                                            Please edit the form')
-        return title
-
-    def clean_slug(self):
-        slug = self.cleaned_data.get('slug')
-
-        for instance in Post.objects.all():
-            if instance.slug == slug:
-                raise forms.ValidationError(slug + ' already exists.\
-                                            Please edit the form')
-        return slug
-
-    def clean_intro(self):
-        intro = self.cleaned_data.get('intro')
-
-        for instance in Post.objects.all():
-            if instance.intro == intro:
-                raise forms.ValidationError('This record already exists.\
-                                            Please edit the form')
-        return intro
-
-    def clean_body(self):
-        body = self.cleaned_data.get('body')
-
-        for instance in Post.objects.all():
-            if instance.body == body:
-                raise forms.ValidationError('This record already exists.\
-                                            Please edit the form')
-        return body
-
 
 class CommentForm(forms.ModelForm):
 
